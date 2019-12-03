bc91e19104852d7938006df2edfd9953dac321b8
Jason M <jbird206@gmail.com>
2019-12-02 20:54:09 -0800

    Add files via upload

---
+++ inputpractice/js/scripts2.js
@@ -0,0 +1,24 @@
+var add = function(beverage, flavor) {
+  return beverage + " and " + flavor;
+};
+
+
+$(document).ready(function() {
+  $("button").click(function(event) {
+    event.preventDefault();
+    var beverage = $("#beverage").val();
+    var flavor = $("input:radio[name=flavor]:checked").val();
+    var result = add(beverage, flavor);
+    var favoriteColor = $("#color").val();
+    var dob = $("#born").val();
+    $(".color").text(favoriteColor);
+    console.log(favoriteColor);
+
+    $(".beverage").text(beverage + " " + result);
+    $(".result").text(result);
+$("#output").text(result);
+    $(".flavor").text(flavor);
+
+    $(".calender").text(dob)
+  });
+});
